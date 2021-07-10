0b4bf1f4871656cdbee457892d2ab42aa4899d4b
Stephen Langevin <slangevin2011@gmail.com>
2021-07-10 14:09:09 -0400

    First test case passing

--- Source/Tutorial_Canvas/Tests/FMyAwesomeTestLD.cpp
+++ Source/Tutorial_Canvas/Tests/FMyAwesomeTestLD.cpp
@@ -18,15 +18,16 @@ uint m_memorySize = 0x4000;
 vector<uint8> rom;
 
 void Setup() {
-	rom(0,m_memorySize);
 	cpu = make_unique<NesCPU>();
 	mmu = make_shared<NesMMU>();
     cpu->AttachMemory(mmu,0x8000); //Set PC to 0x8000*/
+	rom.clear();
 }
 
 bool FMyAwesomeTestLD::RunTest(const FString& Parameters)
 {
 	Setup();
+    rom.resize(0x8000,0);
 	unique_ptr<NesCart> cart = make_unique<NesCart>(rom);
 	cart->Write(0,0xA2);
 	cart->Write(1,0x00);
@@ -34,9 +35,9 @@ bool FMyAwesomeTestLD::RunTest(const FString& Parameters)
 	cpu->X = 0x1; 
 	uint8 cycle = cpu->Tick();
 	TestEqual(TEXT("`Cycle needs to be 2"), cycle, 2);
-	//Assert.AreEqual(0x8002, nesCPU->PC);
-	//Assert.AreEqual(0x0,nesCPU->X);
-	//Assert.AreEqual(0x36,nesCPU->pStateWithBFlag());
+	TestEqual(TEXT("`X needs to be 0"), cpu->X, 0);
+	TestEqual(TEXT("`PC needs to be 0x8002"), cpu->PC, 0x8002);
+	TestEqual(TEXT("`P needs to be 0x36"), cpu->P->pStateWithBFlag(), 0x36);
 	return true;
 }
 
