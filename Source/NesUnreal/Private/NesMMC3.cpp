8a112d81378cc1422505a733022d1b7dce933a13
slangev <slangevin2011@gmail.com>
2022-10-09 10:48:33 -0400

    More MMC3 work

--- Source/NesUnreal/Private/NesMMC3.cpp
+++ Source/NesUnreal/Private/NesMMC3.cpp
@@ -5,6 +5,8 @@
 
 NesMMC3::NesMMC3()
 {
+    ChrBankData.resize(6);
+    PrgBankData.resize(2);
 }
 
 NesMMC3::~NesMMC3()
@@ -13,8 +15,7 @@ NesMMC3::~NesMMC3()
 
 uint8 NesMMC3::Read(unsigned short Address)
 {
-    //TODO: Openbus
-    uint8 returnData = 0x0;
+    uint8 returnData = 0xFF;
     
     // CHR ROM Address
     /*
@@ -47,7 +48,7 @@ uint8 NesMMC3::Read(unsigned short Address)
 
 void NesMMC3::Write(unsigned short Address, uint8 Data)
 {
-    
+    OpenBusData = Data;
     // CHR ROM Address
     if (Address >= 0x0000 && Address <= 0x1FFF) 
     {
@@ -63,11 +64,84 @@ void NesMMC3::Write(unsigned short Address, uint8 Data)
     // PRG ROM Address
     else if(Address >= 0x8000 && Address <= 0xFFFF) 
     {
-        
+        bool bIsEvenAddress = (Address % 2 == 0);
+        // Bank select ($8000-$9FFE, even) / Bank data ($8001-$9FFF, odd)
+        if(Address >= 0x8000 && Address <= 0x9FFF)
+        {
+            if(bIsEvenAddress) 
+            {
+                BankSelect.Write(Data);
+            } 
+            else 
+            {
+                uint8 BankRegister = BankSelect.GetBankRegister();
+                /*
+                    000: R0: Select 2 KB CHR bank at PPU $0000-$07FF (or $1000-$17FF)
+                    001: R1: Select 2 KB CHR bank at PPU $0800-$0FFF (or $1800-$1FFF)
+                    010: R2: Select 1 KB CHR bank at PPU $1000-$13FF (or $0000-$03FF)
+                    011: R3: Select 1 KB CHR bank at PPU $1400-$17FF (or $0400-$07FF)
+                    100: R4: Select 1 KB CHR bank at PPU $1800-$1BFF (or $0800-$0BFF)
+                    101: R5: Select 1 KB CHR bank at PPU $1C00-$1FFF (or $0C00-$0FFF)
+                */
+                if(BankRegister >= 0 && BankRegister <= 5)
+                {
+                    //R0 and R1 ignore the bottom bit, as the value written still counts banks in 1KB units but odd numbered banks can't be selected. 
+                    if(BankRegister == 0 || BankRegister == 1) 
+                    {
+                        Data = Data & 0xFE;
+                    }
+                    ChrBankData.at(BankRegister) = Data;
+                } 
+                else if(BankRegister >= 6 && BankRegister <= 7)
+                {
+                    //R6 and R7 will ignore the top two bits, as the MMC3 has only 6 PRG ROM address lines. Some romhacks rely on an 8-bit extension of R6/7 for oversized PRG-ROM.
+                    PrgBankData.at(BankRegister) = Data & 0x3F;
+                }
+            }
+        }
+
+        // Mirroring ($A000-$BFFE, even) / PRG RAM protect ($A001-$BFFF, odd)
+        if(Address >= 0xA000 && Address <= 0xBFFF)
+        {
+            if(bIsEvenAddress) 
+            {
+
+            } 
+            else 
+            {
+                
+            }
+        }
+
+        // IRQ latch ($C000-$DFFE, even) / IRQ reload ($C001-$DFFF, odd)
+        if(Address >= 0xC000 && Address <= 0xDFFF)
+        {
+            if(bIsEvenAddress) 
+            {
+
+            } 
+            else 
+            {
+                
+            }
+        }
+
+        // IRQ disable ($E000-$FFFE, even) / IRQ enable ($E001-$FFFF, odd)
+        if(Address >= 0xE000 && Address <= 0xFFFF)
+        {
+            if(bIsEvenAddress) 
+            {
+
+            } 
+            else 
+            {
+                
+            }
+        }
     }
 }
 
 uint8 NesMMC3::GetMirrorMode()
 {
-    return 0;
+    return MirrorMode;
 }
--- Source/NesUnreal/Public/NesMMC3.h
+++ Source/NesUnreal/Public/NesMMC3.h
@@ -10,6 +10,30 @@
 /**
  * 
  */
+
+class NesMMC3BankSelectRegister
+{
+public:
+	NesMMC3BankSelectRegister(){};
+	~NesMMC3BankSelectRegister(){};
+	uint8 Read() { return this->Data; };
+	void Write(uint8 InputData) 
+	{ 
+		this->Data = InputData;
+		this->BankRegister = (InputData & 0x7);
+		this->PRGROMBankMode = (InputData & 0x40) >> 6;
+		this->CHRROMBankMode = (InputData & 0x80) >> 7;
+	};
+	uint8 GetBankRegister() { return BankRegister; };
+	uint8 GetPRGROMBankMode() { return PRGROMBankMode; };
+	uint8 GetCHRROMBankMode() { return CHRROMBankMode; };
+private:
+	uint8 Data = 0x00;
+	uint8 BankRegister = 0x00;
+	uint8 PRGROMBankMode = 0x00;
+	uint8 CHRROMBankMode = 0x00;
+};
+
 class NESUNREAL_API NesMMC3 : public NesCartController
 {
 public:
@@ -24,21 +48,22 @@ private:
     std::shared_ptr<std::vector<uint8>> PrgRamMemory;
     std::shared_ptr<std::vector<uint8>> ChrRomMemory;
 	std::shared_ptr<std::vector<uint8>> ChrRamMemory;
-	uint32 ROM_BANK_SIZE_32KB = 0x8000;
-	uint32 ROM_BANK_SIZE_16KB = 0x4000;
-	uint32 ROM_BANK_SIZE_8KB = 0x2000;
-	uint32 ROM_BANK_SIZE_4KB = 0x1000;
+	uint32 BANK_SIZE_8KB = 0x2000;
+	uint32 BANK_SIZE_2KB = 0x800;
+	uint32 BANK_SUZE_1KB = 0x400;
+
 	bool bBatteryBacked = false;
 
 	/*
 		The MMC3 has 4 pairs of registers at $8000-$9FFF, $A000-$BFFF, $C000-$DFFF, and $E000-$FFFF - even addresses ($8000, $8002, etc.) select the low register and odd addresses ($8001, $8003, etc.) select the high register in each pair. These can be broken into two independent functional units: memory mapping ($8000, $8001, $A000, $A001) and scanline counting ($C000, $C001, $E000, $E001). 
 	*/
-	uint8 BankSelect = 0;
-	uint8 BankData = 0;
+	NesMMC3BankSelectRegister BankSelect;
+	std::vector<uint8> ChrBankData; // R0-R5
+	std::vector<uint8> PrgBankData; // R6-R7
 	uint8 MirrorMode = 0;
 	uint8 PrgRamProtect = 0;
 	uint8 IRQLatch = 0;
 	uint8 IRQReload = 0;
-	uint8 IRQDisabled = 0;
-	uint8 IRQEnabled = 0;
+	uint8 OpenBusData = 0;
+	bool bIsIRQEnabled = false;
 };
